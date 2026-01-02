a7fd27dcd11e00249211840626dca98f10da5a36
Homayoun <homayoun763@gmail.com>
2026-01-02 13:53:55 +0330

    add : right project for learning

--- anthology/tailwind.config.ts
+++ anthology/tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx,mdx,html}'],
-  theme: {
-    extend: {},
-  },
+  content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.mdx'],
   plugins: [],
 } satisfies Config;
--- learning/README.md
+++
@@ -1,3 +0,0 @@
-# Anthology
-
-An example repository for Steve's [Building Design Systems with Storybook](https://stevekinney.net/courses/storybook) course for [Frontend Masters](https://frontendmasters.com).
