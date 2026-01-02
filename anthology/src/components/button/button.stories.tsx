c7b8d161182cdae89aa2f2cfa1cc8c7f23342517
Homayoun <homayoun763@gmail.com>
2026-01-02 16:10:15 +0330

    implementing variants

--- anthology/src/components/button/button.stories.tsx
+++ anthology/src/components/button/button.stories.tsx
@@ -4,22 +4,22 @@ import { Button } from './button';
 const meta = {
   title: 'Button',
   component: Button,
-  args : {
-    children : 'Button'
-  }
+  args: {
+    children: 'Button',
+  },
 } satisfies Meta;
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-  args: {
-    style: { backgroundColor: 'blue' },
-  },
+  args : {
+    variant : 'primary'
+  }
 };
 
 export const Secondary: Story = {
   args: {
-    children: 'Secondary',
+    variant : 'secondary'
   },
 };
--- anthology/src/components/button/button.tsx
+++ anthology/src/components/button/button.tsx
@@ -1,3 +1,16 @@
-export const Button = (props: any) => {
-  return <button {...props} />;
+import { ComponentProps } from 'react';
+
+import styles from './button.module.css';
+
+export type BUttonProps = ComponentProps<'button'> & {
+  variant: 'primary' | 'secondary' | 'destructive';
+};
+
+export const Button = ({ variant, ...props }: BUttonProps) => {
+  let className = styles.button;
+
+  if (variant === 'secondary') className += ` ${styles.secondary}`;
+  if (variant === 'destructive') className += ` ${styles.destructive}`;
+
+  return <button className={className} {...props} />;
 };
