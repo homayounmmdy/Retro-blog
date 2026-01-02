cd7c5d6a269c4760a66e59b82514583f1c903e27
Homayoun <homayoun763@gmail.com>
2026-01-03 01:03:32 +0330

    refactor : badge

--- retro-blog/app/components/Badge.tsx
+++ retro-blog/app/components/Badge.tsx
@@ -7,15 +7,13 @@ const Badge = ({ children, active = false }: BadgeProps) => {
   return (
     <div
       className={`${
-        active && "flex items-center space-x-2"
-      } w-fit border border-teal-400 bg-gray-900 px-3 py-1.5`}
+        active && "flex items-center space-x-2 gap-2"
+      } font-mono text-lg font-bold uppercase tracking-wider text-teal-300 drop-shadow-[0_0_6px_rgba(0,255,255,0.4)] w-fit border border-teal-400 bg-gray-900 px-3 py-1.5`}
     >
-      <span className="font-mono text-lg font-bold uppercase tracking-wider text-teal-300 drop-shadow-[0_0_6px_rgba(0,255,255,0.4)]">
-        {children}
-      </span>
       {active && (
         <div className="h-1.5 w-1.5 animate-pulse rounded-full bg-green-400"></div>
       )}
+      {children}
     </div>
   );
 };
